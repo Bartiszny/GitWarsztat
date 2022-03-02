bdc394830d8bc2a3202472aabefe8da811269073
Bartiszny <29127947+Bartiszny@users.noreply.github.com>
2022-03-02 23:55:00 +0100

    Change every odd greeting to answer

--- hello-world.cpp
+++ hello-world.cpp
@@ -3,7 +3,11 @@
 int main() {
 
 	for (short i{0}; i < 10; ++i) {
-		std::cout << "Hello Git!" << std::endl;
+		if (i%2)
+			std::cout << "Hello Friend!" << std::endl;
+		else
+			std::cout << "Hello Git!" << std::endl;
+
 	}
 	return 0;
 }
